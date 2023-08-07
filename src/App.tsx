2e81a7b5ebb05e44dc94eae6ef41d9411f0de798
seikou <seikou615@gmail.com>
2023-08-07 18:59:21 +0100

    Update: added components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { DashboardCard, Header } from "components"
+
+import { dashboards } from "Constants"
 
 function App() {
 
 
   return (
-    <main className="w-screen h-screen">
-      <h1>App</h1>
+    <main className="min-h-screen w-100 pt-8 px-40 pb-16 relative md:px-6 bg-bgLight dark:bg-bgDark ">
+      <section className="absolute inset-0 h-60 bg-topBgLight rounded-b-[1.25rem] dark:bg-topBgDark" />
+      <div className="w-[max-content]  mx-auto z-1 relative flex flex-col gap-[46px] md:gap-24">
+        <Header />
+        
+        <div className="w-[max-content] grid grid-cols-4 gap-[30px]  md:grid-cols-2 sm:flex sm:flex-col sm:w-full">
+          {dashboards.map(dashboard => (
+            <DashboardCard key={dashboard.company} {...dashboard} />
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
