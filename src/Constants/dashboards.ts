553adc53bbc11ddc0cb53030fef445ae56920986
seikou <seikou615@gmail.com>
2023-08-07 16:58:45 +0100

    Update: Refactored with dashboard type

--- src/Constants/dashboards.ts
+++ src/Constants/dashboards.ts
@@ -1,27 +1,33 @@
-const dashboards = [
-    {
-        company: "facebook",
-        followers: "1987",
-        today: 12
-    },
+import { IDashboard } from "Types";
 
-    {
-        company: "twitter",
-        followers: "1044",
-        today: 99
-    },
-    
-    {
-        company: "instagram",
-        followers: "11k",
-        today: 1099
-    },
+const dashboards: IDashboard[] = [
+  {
+    username: "nathanf",
+    company: "facebook",
+    followers: "1987",
+    today: 12,
+  },
 
-    {
-        company: "youtube",
-        followers: "8239",
-        today: -144
-    }
-]
+  {
+    username: "nathanf",
+    company: "twitter",
+    followers: "1044",
+    today: 99,
+  },
 
-export { dashboards }
+  {
+    username: "realnathanf",
+    company: "instagram",
+    followers: "11k",
+    today: 1099,
+  },
+
+  {
+    username: "Nathan F.",
+    company: "youtube",
+    followers: "8239",
+    today: -144,
+  },
+];
+
+export { dashboards };
