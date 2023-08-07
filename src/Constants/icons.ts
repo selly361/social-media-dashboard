f4e9ae44a2bf45f500e4f75f5fd27357b3c891a9
seikou <seikou615@gmail.com>
2023-08-07 15:34:33 +0100

    Add: `icons` constant

---
+++ src/Constants/icons.ts
@@ -0,0 +1,10 @@
+import { Facebook, Instagram, Twitter, Youtube } from "Icons";
+
+const icons = {
+    "twitter": Twitter,
+    "facebook": Facebook,
+    "instagram": Instagram,
+    "youtube": Youtube
+}
+
+export {  icons }
