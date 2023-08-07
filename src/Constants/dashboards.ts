98cbd07696773bd97406a2ce9ee3611b63ff8bcf
seikou <seikou615@gmail.com>
2023-08-07 15:34:24 +0100

    Add: `dashboards` constant

---
+++ src/Constants/dashboards.ts
@@ -0,0 +1,27 @@
+const dashboards = [
+    {
+        company: "facebook",
+        followers: "1987",
+        today: 12
+    },
+
+    {
+        company: "twitter",
+        followers: "1044",
+        today: 99
+    },
+    
+    {
+        company: "instagram",
+        followers: "11k",
+        today: 1099
+    },
+
+    {
+        company: "youtube",
+        followers: "8239",
+        today: -144
+    }
+]
+
+export { dashboards }
