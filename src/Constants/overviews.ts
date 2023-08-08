b5ebbe0849b03d6101d7164e53626f7a69ea11c7
seikou <seikou615@gmail.com>
2023-08-08 11:27:45 +0100

    Add: `overviews` constant

---
+++ src/Constants/overviews.ts
@@ -0,0 +1,61 @@
+import { IOverview } from "Types"
+
+const overviews: IOverview[] = [
+  {
+    company: "facebook",
+    type: "Page Views",
+    platformSpecificValue: "87",
+    percentage: 3,
+  },
+
+  {
+    company: "facebook",
+    type: "Likes",
+    platformSpecificValue: "52",
+    percentage: -2,
+  },
+
+  {
+    company: "facebook",
+    type: "Likes",
+    platformSpecificValue: "5462",
+    percentage: 2257,
+  },
+
+  {
+    company: "instagram",
+    type: "Profile Views",
+    platformSpecificValue: "52k",
+    percentage: 1375,
+  },
+
+  {
+    company: "twitter",
+    type: "Retweets",
+    platformSpecificValue: "117",
+    percentage: 303,
+  },
+
+  {
+    company: "twitter",
+    type: "Likes",
+    platformSpecificValue: "507",
+    percentage: 553,
+  },
+
+  {
+    company: "youtube",
+    type: "Likes",
+    platformSpecificValue: "107",
+    percentage: -19,
+  },
+
+  {
+    company: "youtube",
+    type: "Total Views",
+    platformSpecificValue: "1407",
+    percentage: -12,
+  }
+]
+
+export { overviews }
